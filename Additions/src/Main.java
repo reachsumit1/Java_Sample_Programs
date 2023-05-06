f2886fd6ca327a6ec7dc372c0ee3f564c7612976
“reachsumit1” <1057185138m@gmail.com>
2023-05-06 20:16:13 +0530

    Practices

---
+++ Additions/src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+class add {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        int a,b,c;
+        a=14;
+        b=12;
+        c=a+b;
+        System.out.println("Additon of two numbers = "+c);
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+
+    }
+}
---
+++ Prime1/src/Main.java
@@ -0,0 +1,21 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+class Prime {
+    public static void main(String args[])
+    {
+        int num=20, count;
+        for (int i=1;i<=num; i++){
+            count=0;
+            for (int j=2;j<=i/2;j++) {
+                if (i % j == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if (count ==0)
+            {
+                System.out.println(i);
+                }
+            }
+        }
+    }
